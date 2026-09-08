333321e1ce471d731c02e7c976eec5d87419ff1e
Reyder Alvarado <reyder.leo@gmail.com>
2026-09-08 10:35:48 -0500

    feat: add OutlinedTextField component

--- app/src/main/java/com/example/lab04/MainActivity.kt
+++ app/src/main/java/com/example/lab04/MainActivity.kt
@@ -23,7 +23,7 @@ class MainActivity : ComponentActivity() {
                     WelcomeCard(
                         modifier = Modifier.padding(innerPadding)
                     )
-                    )
+
                 }
             }
         }
--- app/src/main/java/com/example/lab04/WelcomeCard.kt
+++ app/src/main/java/com/example/lab04/WelcomeCard.kt
@@ -6,13 +6,20 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Card
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 
 @Composable
 fun WelcomeCard(modifier: Modifier = Modifier) {
+    var movieName by remember { mutableStateOf("") }
+
     Card(
         modifier = modifier.padding(16.dp)
     ) {
@@ -28,6 +35,14 @@ fun WelcomeCard(modifier: Modifier = Modifier) {
 
             Text(
                 text = "Hello, Student!"
+            )
+
+            Spacer(modifier = Modifier.height(16.dp))
+
+            OutlinedTextField(
+                value = movieName,
+                onValueChange = { movieName = it },
+                label = { Text("Movie Name") }
             )
         }
     }
