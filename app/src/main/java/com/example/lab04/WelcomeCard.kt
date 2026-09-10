6d72e9cc6361e7596e7aefdca8efde66d08b1d99
Reyder Alvarado <reyder.leo@gmail.com>
2026-09-10 15:28:13 -0500

    feat: update welcome message as Reyder

--- app/src/main/java/com/example/lab04/WelcomeCard.kt
+++ app/src/main/java/com/example/lab04/WelcomeCard.kt
@@ -38,7 +38,7 @@ fun WelcomeCard(modifier: Modifier = Modifier) {
             modifier = Modifier.padding(16.dp)
         ) {
             Text(
-                text = "Welcome to the Course!",
+                text = "Bienvenido - Reyder",
                 style = MaterialTheme.typography.headlineSmall
             )
 
